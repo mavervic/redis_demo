83c57c4a2779e982e3a2ec2de7600727b5d52a8f
Vic.Liu <sunset1117@hotmail.com>
2024-09-16 17:32:13 +0800

    style: update the JSON paths for the array, object array, and object fields.

--- src/example/jedis/types/json/BasicInsertExample.java
+++ src/example/jedis/types/json/BasicInsertExample.java
@@ -16,25 +16,25 @@ public class BasicInsertExample {
 
 		// ["10","20","30"]
 		List<String> array = MockDataUtils.getArray();
-		jedis.jsonSet("array", gson.toJson(array));
+		jedis.jsonSet("json:array:normal.array", gson.toJson(array));
 
 		// [{"name":"vic"},{"name":"david"},{"name":"peter"}]
 		List<Object> objArray = MockDataUtils.getObjArray();
-		jedis.jsonSet("objArray", gson.toJson(objArray));
+		jedis.jsonSet("json:array:wrap.obj", gson.toJson(objArray));
 
 		// {"boolean_true":true,"boolean_false":false,"string":"hello world","number":123}
 		Map<String, Object> obj = MockDataUtils.getObj();
-		jedis.jsonSet("obj", gson.toJson(obj));
+		jedis.jsonSet("json:obj:normal.obj", gson.toJson(obj));
 
 		// {"boolean_true":true,"boolean_false":false,"string":"hello world","number":123,"array":["10","20","30"]}
 		Map<String, Object> objContentArray = MockDataUtils.getObjContentArray();
-		jedis.jsonSet("objContentArray", gson.toJson(objContentArray));
+		jedis.jsonSet("json:obj:field.has.array", gson.toJson(objContentArray));
 
 		// {"boolean_true":true,"boolean_false":false,"string":"hello world","number":123,
 		// "array":["10","20","30"],
 		// "objArray":[{"name":"vic"},{"name":"david"},{"name":"peter"}]}
 		Map<String, Object> objContentArrayAndObjArray = MockDataUtils.getObjContentArrayAndObjArray();
-		jedis.jsonSet("objContentArrayAndObjArray", gson.toJson(objContentArrayAndObjArray));
+		jedis.jsonSet("json:obj:complex.array", gson.toJson(objContentArrayAndObjArray));
 
 	}
 }
--- src/example/jedis/types/json/BasicUpdateExample.java
+++ src/example/jedis/types/json/BasicUpdateExample.java
@@ -17,37 +17,37 @@ public class BasicUpdateExample {
 
 		// ["10","20","30"] => ["10","20","30","40","50","60"]
 		List<String> array = MockDataUtils.getArray();
-		jedis.jsonSet("array", gson.toJson(array));
-		jedis.jsonArrAppend("array", Path2.ROOT_PATH, "\"40\"", "\"50\"", "\"60\"");
+		jedis.jsonSet("json:array:normal.array", gson.toJson(array));
+		jedis.jsonArrAppend("json:array:normal.array", Path2.ROOT_PATH, "\"40\"", "\"50\"", "\"60\"");
 
 		// [{"name":"vic"},{"name":"david"},{"name":"peter"}] =>
 		// [{"name":"vic"},{"name":"david"},{"name":"peter"},{"name":"Ben"},{"name":"Ann"}]
 		// [{"name":"vic","hello":"world"},{"name":"david","hello":"world"},{"name":"peter","hello":"world"},{"name":"Ben","hello":"world"},{"name":"Ann","hello":"world"}]
 		List<Object> objArray = MockDataUtils.getObjArray();
-		jedis.jsonSet("objArray", gson.toJson(objArray));
-		jedis.jsonArrAppend("objArray", Path2.ROOT_PATH, "{\"name\":\"Ben\"}", "{\"name\":\"Ann\"}");
-		jedis.jsonMerge("objArray", Path2.of(".[*]"), "{\"hello\":\"world\"}");
+		jedis.jsonSet("json:array:wrap.obj", gson.toJson(objArray));
+		jedis.jsonArrAppend("json:array:wrap.obj", Path2.ROOT_PATH, "{\"name\":\"Ben\"}", "{\"name\":\"Ann\"}");
+		jedis.jsonMerge("json:array:wrap.obj", Path2.of(".[*]"), "{\"hello\":\"world\"}");
 		
 		// {"boolean_true":true,"boolean_false":false,"string":"hello world","number":123} =>
 		// {"boolean_true":true,"boolean_false":false,"string":"hello world","number":123, "hello": "world"}
 		Map<String, Object> obj = MockDataUtils.getObj();
-		jedis.jsonSet("obj", gson.toJson(obj));
-		jedis.jsonMerge("obj", Path2.ROOT_PATH, "{\"hello\":\"world\"}");
+		jedis.jsonSet("json:obj:normal.obj", gson.toJson(obj));
+		jedis.jsonMerge("json:obj:normal.obj", Path2.ROOT_PATH, "{\"hello\":\"world\"}");
 
 		// {"boolean_true":true,"boolean_false":false,"string":"hello world","number":123,"array":["10","20","30"]} =>
 		// {"boolean_true":true,"boolean_false":false,"string":"hello world","number":123,"array":["10","20","30","5","10","15"],"array2":["100","200","300"]}
 		Map<String, Object> objContentArray = MockDataUtils.getObjContentArray();
-		jedis.jsonSet("objContentArray", gson.toJson(objContentArray));
-		jedis.jsonMerge("objContentArray", Path2.ROOT_PATH, "{\"array2\":[\"100\",\"200\",\"300\"]}");
-		jedis.jsonArrAppend("objContentArray", Path2.of(".array"), "\"5\"", "\"10\"", "\"15\"");
+		jedis.jsonSet("json:obj:field.has.array", gson.toJson(objContentArray));
+		jedis.jsonMerge("json:obj:field.has.array", Path2.ROOT_PATH, "{\"array2\":[\"100\",\"200\",\"300\"]}");
+		jedis.jsonArrAppend("json:obj:field.has.array", Path2.of(".array"), "\"5\"", "\"10\"", "\"15\"");
 
 		// {"boolean_true":true,"boolean_false":false,"string":"hello world","number":123,
 		// "array":["10","20","30"],
 		// "objArray":[{"name":"vic"},{"name":"david"},{"name":"peter"}]} =>
 		// "objArray":[{"name":"vic","hello":"world"},{"name":"david","hello":"world"},{"name":"peter","hello":"world"}]} =>
 		Map<String, Object> objContentArrayAndObjArray = MockDataUtils.getObjContentArrayAndObjArray();
-		jedis.jsonSet("objContentArrayAndObjArray", gson.toJson(objContentArrayAndObjArray));
-		jedis.jsonMerge("objContentArrayAndObjArray", Path2.of(".objArray[*]"), "{\"hello\":\"world\"}");
+		jedis.jsonSet("json:obj:complex.array", gson.toJson(objContentArrayAndObjArray));
+		jedis.jsonMerge("json:obj:complex.array", Path2.of(".objArray[*]"), "{\"hello\":\"world\"}");
 
 	}
 }
