c7edc58a3d69d42cc45d2eb4a9e10c38eb47fc1d
Vic.Liu <sunset1117@hotmail.com>
2024-10-03 12:44:29 +0800

    refactor: Remove unnecessary comments and adjust code formatting in SortedSetsExample

--- src/example/jedis/types/zset/SortedSetsExample.java
+++ src/example/jedis/types/zset/SortedSetsExample.java
@@ -14,7 +14,6 @@ public class SortedSetsExample {
 	public static void main(String[] args) {
 		JedisPooled jedis = RedisUtils.getJedisPooled();
 
-		
 		/*
 		 * 添加賽車手們在第一場比賽中獲得的分數
 		 */
@@ -23,7 +22,7 @@ public class SortedSetsExample {
 
 		long res2 = jedis.zadd("racer_scores", 12D, "Castilla");
 		System.out.println(res2); // >>> 1
-
+		// 這邊使用了 Java 8 的新特性，稱為 Double Brace Initialization，總之就是一種初始化實體方式，這邊直接初始化了 map
 		long res3 = jedis.zadd("racer_scores", new HashMap<String, Double>() {
 			{
 				put("Sam-Bodden", 8D);
@@ -34,9 +33,7 @@ public class SortedSetsExample {
 			}
 		});
 		System.out.println(res3); // >>> 4
-		
-		
-		
+
 		/*
 		 * 取得排序
 		 * 
@@ -48,31 +45,25 @@ public class SortedSetsExample {
 		List<String> res5 = jedis.zrevrange("racer_scores", 0, -1);
 		System.out.println(res5); // >>> [Prickett, Castilla, Castil, Royce, Norem, Sam-Bodden, Ford]
 
-		
-		
-		
 		/*
 		 * 同時取得所有排名以及分數
 		 */
 		List<Tuple> res6 = jedis.zrangeWithScores("racer_scores", 0, -1);
-		System.out.println(res6); // [[Ford,6.0], [Sam-Bodden,9.0], [Norem,10.0], [Royce,10.0], [Castilla,12.0], [Prickett,14.0]]
+		System.out.println(res6); // [[Ford,6.0], [Sam-Bodden,9.0], [Norem,10.0], [Royce,10.0], [Castilla,12.0],
+									// [Prickett,14.0]]
 
-		
 		/*
 		 * 取得最小正整數 ~ 10分的選手們
 		 */
 		List<String> res7 = jedis.zrangeByScore("racer_scores", Double.MIN_VALUE, 10D);
 		System.out.println(res7); // >>> [Ford, Sam-Bodden, Norem, Royce]
 
-		
-		
-
 		/**
 		 * 移除掉 Castilla
 		 */
 		long res8 = jedis.zrem("racer_scores", "Castilla");
 		System.out.println(res8); // >>> 1
-		
+
 		/*
 		 * 篩選掉最小正整數 ~ 9分的選手們
 		 */
@@ -85,21 +76,18 @@ public class SortedSetsExample {
 		List<String> res10 = jedis.zrange("racer_scores", 0, -1);
 		System.out.println(res10); // >>> [Norem, Royce, Prickett]
 
-		
 		/*
 		 * 取得元素所在位置 (由前至後)
 		 */
 		long res11 = jedis.zrank("racer_scores", "Norem");
 		System.out.println(res11); // >>> 0
 
-		
 		/*
 		 * 取得元素所在位置 (由後至前)，可以用在取得遊戲分數的排名
 		 */
 		long res12 = jedis.zrevrank("racer_scores", "Norem");
 		System.out.println(res12); // >>> 2
 
-		
 		/*
 		 * redis 對於同分項會依照字母順序做排序
 		 */
@@ -119,32 +107,37 @@ public class SortedSetsExample {
 		List<String> res14 = jedis.zrange("lexicographical_scores", 0, -1);
 		System.out.println(res14); // >>> [A, a, aAa, b, c, d, e]
 
-		// 搜尋開頭為特定字串的元素
-		List<String> res15 = jedis.zrangeByLex("lexicographical_scores", "[A", "[L");
-		System.out.println(res15); // >>> [A]
-
-		
-		
-		
-		
-		
-		/*
-		 * FB 遊戲排行榜
+		/**
+		 * 取得字串開頭為 A ~ c 之間的元素
 		 * 
-		 * 如果我們知道賽車手的新分數，我們可以直接透過ZADD命令更新分數
+		 * 這邊使用了 ZRANGEBYLEX 這個指令，這個指令是用來取得字典序在某個範圍內的元素 (區分大小寫)
 		 * 
-		 * 反過來說，如果我們只知道得分不知道總分，則我們可以使用ZINCRBY命令。
+		 * 第二個參數表示搜尋起點條件
+		 * 
+		 * 第三個參數表示搜尋終點條件
+		 * 
+		 * `[` 表示包含、`(` 表示不包含
 		 */
-		long res16 = jedis.zadd("fb_game_scores", 100D, "Wood");
-		System.out.println(res16); // >>> 1
-		long res17 = jedis.zadd("fb_game_scores", 100D, "Henshaw");
-		System.out.println(res17); // >>> 1
-		long res18 = jedis.zadd("fb_game_scores", 100D, "Henshaw");
-		System.out.println(res18); // >>> 0
+		List<String> res15 = jedis.zrangeByLex("lexicographical_scores", "[A", "[c");
+		System.out.println(res15); // >>> [A, a, aAa, b, c]
 
-		double res19 = jedis.zincrby("fb_game_scores", 50D, "Wood");
+		/**
+		 * 遊戲計分通常有處理方式
+		 * 
+		 * 1. 直接覆蓋最新分數
+		 * 
+		 * 2. 累加分數
+		 */
+		long res16 = jedis.zadd("video_game_scores", 100D, "Wood");
+		System.out.println(res16); // >>> 1
+		long res17 = jedis.zadd("video_game_scores", 100D, "Henshaw");
+		System.out.println(res17); // >>> 1
+		long res18 = jedis.zadd("video_game_scores", 100D, "Henshaw");
+		System.out.println(res18); // >>> 0
+		// 累加分數
+		double res19 = jedis.zincrby("video_game_scores", 50D, "Wood");
 		System.out.println(res19); // >>> 150.0
-		double res20 = jedis.zincrby("fb_game_scores", 100D, "Henshaw");
+		double res20 = jedis.zincrby("video_game_scores", 100D, "Henshaw");
 		System.out.println(res20); // >>> 200.0
 	}
 }
